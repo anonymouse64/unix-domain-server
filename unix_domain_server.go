1735a158232323dca382031537744fa6ccacbbf0
Tony Espy <espy@canonical.com>
2018-01-04 15:33:47 -0500

    Modify server to test timedatectl

--- unix_domain_server.go
+++ unix_domain_server.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/exec"
 )
 
 func echoServer(c net.Conn) {
@@ -30,6 +31,39 @@ func main() {
 	if len(os.Args) <= 1 {
 		log.Fatal("Usage unix-domain-socket <socketdir>")
 	}
+
+	// Now run timedate-ctrl twice enable/disable
+	cmd := exec.Command("timedatectl", "set-ntp", "true")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("timedatectl error cmd #1: %v", err)
+	}
+
+	cmd = exec.Command("timedatectl", "set-ntp", "false")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("timedatectl error cmd #2: %v", err)
+	}
+
+	cmd = exec.Command("timedatectl", "status")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("timedatectl error cmd #3: %v", err)
+	}
+
+	cmd = exec.Command("timedatectl", "set-timezone", "America/Tijuana")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("timedatectl error cmd #4: %v", err)
+	}
+
+	cmd = exec.Command("timedatectl", "set-time", "2017-10-31 18:17:16")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("timedatectl error cmd #5: %v", err)
+	}
+
+	fmt.Println("timedatectl all seemed to work")
 
 	dir := os.Args[1]
 	path := dir + "/sockets/socket"
